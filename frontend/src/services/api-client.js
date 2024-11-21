322cb2814bdd36b9b8b2924a543d406e5d0f3480
SAwandya <sachilaawandya@gmail.com>
2024-11-21 17:45:04 +0530

    deploy 2

--- frontend/src/services/api-client.js
+++ frontend/src/services/api-client.js
@@ -1,7 +1,7 @@
 import axios, { CanceledError } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `http://azure-reservation-app.azurewebsites.net/api`,
+  baseURL: `https://azure-reservation-app.azurewebsites.net/api`,
 });
 
 class APIClient {
